30516ec7b0d098b01e5291be36b7629cebf2c238
dmitri <dymkatrakee@gmail.com>
2022-06-13 18:02:42 +0300

    Initial Commit

---
+++ KryptosGraphein/KryptosGrapheinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KryptosGrapheinApp.swift
+//  KryptosGraphein
+//
+//  Created by Dumitru Paraschiv on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct KryptosGrapheinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
